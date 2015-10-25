b06fdd2e2b2d897a9e3b838d5cbce1f99766ca8a
swapnilnakate7 <nakate.swapnil7@gmail.com>
2015-10-26 05:15:15 +0530

    Update script.js

--- edusrch/js/script.js
+++ edusrch/js/script.js
@@ -1,6 +1,6 @@
 (function () {
   var script = document.createElement("script");
   script.type = "text/javascript";
-  script.src  = "MathJax.js?config=AM_HTMLorMML-full";
+  script.src  = /"MathJax.js?config=AM_HTMLorMML-full";
   document.getElementsByTagName("head")[0].appendChild(script);
 })();
